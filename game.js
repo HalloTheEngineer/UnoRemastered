ae2e200cb13e437441f75248696632c613379bc3
HalloTheEngineer <83722147+HalloTheEngineer@users.noreply.github.com>
2023-10-20 14:31:59 +0200

    Backup 2

--- game.js
+++ game.js
@@ -3,9 +3,13 @@ let tag2 = "";
 let cardCount = 7;
 let gameHandler;
 let cardHandler;
+let turnHandler;
+let colorHandler;
 let allCards = [];
 let currentPlayer;
 let shownCard;
+let forcedColorCache;
+let forcedColor;
 let cardsPlayerOne = [];
 let cardsPlayerTwo = [];
 
@@ -21,6 +25,9 @@ window.onload = () => {
 
     $("#loader").fadeOut("center");
 
+    colorHandler = new ColorHandler();
+    turnHandler = new TurnHandler();
+    turnHandler.init();
     cardHandler = new CardHandler();
     gameHandler = new GameHandler();
     gameHandler.startGame();
@@ -28,12 +35,61 @@ window.onload = () => {
 
 //JQuery - Events
 $(document).ready(() => {
-    $("#deck1").on("click", () => {
-        new bootstrap.Offcanvas("#offcanvasTop").show();
+    $("#deck1").on({
+        click: () => {
+            new bootstrap.Offcanvas("#offT").show();
+        },
+        dragover: (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        },
+        dragenter: (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        },
+        drop: (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            if (currentPlayer === 1) {
+                cardHandler.drawCard(1);
+                turnHandler.next();
+            }
+        }
     })
-    $("#deck2").on("click", () => {
-        new bootstrap.Offcanvas("#offcanvasBottom").show();
+    $("#deck2").on({
+        click: () => {
+            new bootstrap.Offcanvas("#offB").show();
+        },
+        dragover: (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        },
+        dragenter: (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        },
+        drop: (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            if (currentPlayer === 2) {
+                cardHandler.drawCard(2);
+                turnHandler.next();
+            }
+        }
     })
+    $("#chooseRed").on("click", () => {
+        colorHandler.handleButton(Card.redColor);
+    })
+    $("#chooseYellow").on("click", () => {
+        colorHandler.handleButton(Card.yellowColor);
+    })
+    $("#chooseGreen").on("click", () => {
+        colorHandler.handleButton(Card.greenColor);
+    })
+    $("#chooseBlue").on("click", () => {
+        colorHandler.handleButton(Card.blueColor);
+    })
+
 })
 
 //Classes
@@ -43,42 +99,172 @@ class GameHandler {
     startGame() {
         cardHandler.collectCards();
         cardHandler.distributeCards();
-        console.log(cardsPlayerOne, cardsPlayerTwo)
-        console.log(allCards)
+        cardHandler.chooseStarterCard();
+
+        $("#offTTitle").html(`Player: ${tag1}`);
+        $("#offBTitle").html(`Player: ${tag2}`);
+
+        $("#startingPlayer").html(`<strong>${turnHandler.getPlayerTag(currentPlayer)}</strong> is going to start!`)
+        new bootstrap.Modal("#startingInfo").show();
     }
 }
 class CardHandler {
     constructor() {
+    }
+    collectCards() {
+        for (let i = 0; i < 14; i++) {
+            for (let j = 0; j < 8; j++) {
+                allCards.push(new Card(i, j));
+            }
+        }
+        allCards.splice(4, 4);
     }
     distributeCards() {
         for (let i = 0; i < cardCount; i++) {
             moveItem(allCards, cardsPlayerOne, allCards[Math.floor(Math.random() * allCards.length)]);
             moveItem(allCards, cardsPlayerTwo, allCards[Math.floor(Math.random() * allCards.length)]);
         }
+        cardsPlayerOne.forEach(value => this.addCardToPlayerDeck(value, value.uuid, 1));
+        cardsPlayerTwo.forEach(value => this.addCardToPlayerDeck(value, value.uuid, 2));
     }
-    updateShownCard() {
-        //$("#gameStack").style.top = "-"+shownCard.
-    }
-    collectCards() {
-        for (let i = 0; i < 14; i++) {
-            for (let j = 0; j < 8; j++) {
-                allCards.push(new Card(i, j))
-            }
+    addCardToPlayerDeck(card, uuid, player) {
+        const html = `<li><div class='gallery-entry-pl2' id=${uuid} style='background-image: url("assets/uno_spritesheet.png"); background-position: -${card.x}px -${card.y}px'></div></li>`
+        if (player === 1) {
+            $("#deck1Cards").append(html);
+            $(`#${uuid}`).on("click", ev => this.handleCardPick(ev, player));
+        } else if (player === 2) {
+            $("#deck2Cards").append(html);
+            $(`#${uuid}`).on("click", ev => this.handleCardPick(ev, player));
         }
-        allCards.splice(4, 4)
-        console.log(allCards)
     }
     chooseStarterCard() {
-
+        let card = allCards[Math.floor(Math.random() * allCards.length)];
+        while (card.getType() !== "regular") {
+            card = allCards[Math.floor(Math.random() * allCards.length)];
+        }
+        shownCard = card;
+        $("#gameStack").css("background-position", "-"+card.x+"px -" + card.y + "px");
+    }
+    updateShownCard(card) {
+        $("#gameStack").css("background-position", `-${card.x}px -${card.y}px`);
+        shownCard = card;
     }
     drawCard(player) {
+        let card = allCards[Math.floor(Math.random() * allCards.length)];
         if (player === 1) {
-            moveItem(allCards, cardsPlayerOne, allCards[Math.floor(Math.random() * allCards.length)]);
+            moveItem(allCards, cardsPlayerOne, card);
+            this.addCardToPlayerDeck(card, card.uuid, player);
         } else if (player === 2) {
-            moveItem(allCards, cardsPlayerTwo, allCards[Math.floor(Math.random() * allCards.length)]);
+            moveItem(allCards, cardsPlayerTwo, card);
+            this.addCardToPlayerDeck(card, card.uuid, player);
         }
     }
+    async handleCardPick(event, player) {
+        let card;
 
+        if (player !== currentPlayer) return;
+
+        if (player === 1) card = cardsPlayerOne.filter(value => value.uuid === event.target.id)[0];
+        else if (player === 2) card = cardsPlayerTwo.filter(value => value.uuid === event.target.id)[0];
+
+        if (card.getType() === "special" || card.getType() === "drawFour") {
+            new bootstrap.Modal("#colorPicker").show();
+            await colorHandler.awaitColorPick().then(value => {
+                forcedColor = value
+                console.log(value)
+            });
+        }
+
+        if (!this.validateAction(card, player, forcedColor)) return;
+
+        this.updateShownCard(card);
+        $(`#${card.uuid}`).parent().remove();
+    }
+    validateAction(card, player, forcedColor) {
+        switch (shownCard.getType()) {
+            case "regular":
+                if ((!card.hasColor() || shownCard.getColor() === card.getColor()) || (shownCard.getNumber() === card.getNumber())) {
+                    turnHandler.next();
+                    return true;
+                } else return false;
+            case "skip":
+                if (shownCard.getColor() === card.getColor() || !card.hasColor()) return true;
+                break;
+            case "mirror":
+                if (shownCard.getColor() === card.getColor() || !card.hasColor()) return true;
+                break;
+            case "drawTwo":
+                if (shownCard.getType() !== "drawTwo" || shownCard.getColor() !== card.getColor()) {
+                    for (let i = 0; i < 2; i++) {
+                        this.drawCard(turnHandler.getNextPlayer(player))
+                    }
+                    turnHandler.next();
+                    return true;
+                } else return false;
+            case "drawFour":
+                if (forcedColor !== undefined && card.getColor() === forcedColor) {
+                    for (let i = 0; i < 4; i++) {
+                        this.drawCard(turnHandler.getNextPlayer(player));
+                        console.log("add")
+                    }
+                    turnHandler.next();
+                    return true;
+                } else return false;
+            case "special":
+                if (card.getColor() === forcedColor) {
+                    turnHandler.next();
+                    return true;
+                } else return false;
+        }
+    }
+}
+class ColorHandler {
+    handleButton(color) {
+        forcedColorCache = color;
+    }
+    awaitColorPick() {
+        return new Promise(resolve => {
+            setInterval(()=>{
+                if (forcedColorCache != null) {
+                    resolve(forcedColorCache);
+                    forcedColorCache = null;
+                }
+            }, 50); //20 TPS
+        })
+    }
+}
+class TurnHandler {
+    static player1 = 1;
+    static player2 = 2;
+    randomPlayer() {
+        if (Math.random() >= 0.5) return TurnHandler.player1;
+        else return TurnHandler.player2;
+    }
+    init() {
+        currentPlayer = this.randomPlayer();
+        this.updateBorder();
+    }
+    next() {
+        currentPlayer = this.getNextPlayer(currentPlayer);
+        this.updateBorder();
+    }
+    getPlayerTag(player) {
+        if (player === 1) return tag1;
+        else if (player === 2) return tag2;
+    }
+    getNextPlayer(player) {
+        if (player === 1) return 2;
+        else if (player === 2) return 1;
+    }
+    updateBorder() {
+        if (currentPlayer === 1) {
+            $("#deck1").css("border-color", "slategray");
+            $("#deck2").css("border-color", "transparent");
+        } else if (currentPlayer === 2) {
+            $("#deck1").css("border-color", "transparent");
+            $("#deck2").css("border-color", "slategray");
+        }
+    }
 }
 class Card {
     static regularCard = "regular";
@@ -93,12 +279,12 @@ class Card {
     static greenColor = "green";
     static blueColor = "blue";
     constructor(indexX, indexY) {
+        this.uuid = generateUUID();
         this.column = indexX;
         this.row = indexY;
         this.x = indexX * 137;
         this.y = indexY * 206;
-        this.width = 137;
-        this.height = 206;
+        this.type = this.getType();
     }
     getType() {
         switch (this.column) {
@@ -144,6 +330,9 @@ class Card {
         } else return null;
 
     }
+    hasColor() {
+        return this.getColor() != null;
+    }
     getNumber() {
         if (this.column < 10) {
             return this.column;
@@ -163,4 +352,7 @@ function isNumeric(str) {
 function moveItem(source, target, item) {
     source.splice(allCards.indexOf(item), 1);
     target.push(item);
+}
+function generateUUID() {
+    return crypto.randomUUID();
 }
